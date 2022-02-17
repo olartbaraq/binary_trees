39ca7d729329e69591246c27ed8433e73fe84382
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-17 21:34:03 +0100

    fixed the null pointer name

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,7 +8,7 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-if (node == NULL || tree->parent == NULL)
+if (tree == NULL || tree->parent == NULL)
 return (0);
 return (1 + binary_tree_depth(tree->parent));
 }
