826288504bcf93c774f124f178e503a431bf8864
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-17 18:45:02 +0100

    fixed inorder traversal and first try post order

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,16 +3,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - in-order traverse of binary tree
+ * binary_tree_postorder - post-order traverse of binary tree
  * @tree: starting point of traversal
  * @func: function to call on the value at each node
  */
 
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (tree == NULL || func == NULL)
 return;
-binary_tree_inorder(tree->left, func);
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
 func(tree->n);
-binary_tree_inorder(tree->right, func);
 }
